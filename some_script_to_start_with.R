71a06bd153aa45a24c8df397f7380c820b64a972
Tomasz Samsel <samsel.tomasz@gmail.com>
2015-10-26 14:09:04 +0000

    Random forest didnt work as fast as expected. Missed deadline for assigment.. Better luck next month :) Still working on the project though. The script is fully functional now. Text files for all 20 problems have been generated, and are ready to upload. Start working on the paper.

--- some_script_to_start_with.R
+++ some_script_to_start_with.R
@@ -35,7 +35,51 @@ my_training_2 <- my_training[, colSums(is.na(training)) == 0]
 ## remove first 7 columns as they have no predictive value
 my_training_2 <- my_training_2[, -c(1:7)]
 
+##------------now first model - decision tree----------
 
+## fit the first model: rpart
+## mod_fit_1 <- rpart(classe ~ ., data=my_training_2, method="class")
+mod_fit_2 <- train(classe ~ ., method="rpart", data=my_training_2)
+
+## lets have a look how it perform
+print(mod_fit_2$finalModel)
+        ## nice :)
+fancyRpartPlot(mod_fit_2$finalModel)
+
+## predict and check accuracy
+predictions_1 <- predict(mod_fit_2, newdata=my_testing)
+confusionMatrix(predictions_1, my_testing$classe)
+        ## very low accuracy, only 49%
+
+## ------------ second model - random forests ---------
+
+## fit the random forest model insetad, should be a bit better :)
+## mod_fit_3 <- train(classe ~ ., method="rf", data=my_training_2, prox=TRUE, ntree=5)
+## takes for ages to compute, over 8h... :\ missed assigment because of this..
+mod_fit_3 <- randomForest(classe ~ ., data=my_training_2)
+## much faster then caret package, although returns error on my machine when used
+## with proximity=TRUE option. Not enought ram...
+
+## do the prediction 
+prediction_2 <- predict(mod_fit_3, newdata=my_testing)
+confusionMatrix(prediction_2, my_testing$classe)
+        ## excelent, 99.39% accuracy :)
+
+## predict answers for file submission
+answers <- predict(mod_fit_3, newdata=testing)
+answers
+
+## --- assigment submission files ----
+pml_write_files = function(x){
+        n = length(x)
+        for(i in 1:n){
+                filename = paste0("problem_id_",i,".txt")
+                write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+        }
+}
+
+pml_write_files(answers)
+        ## worked :)
 
 
 
